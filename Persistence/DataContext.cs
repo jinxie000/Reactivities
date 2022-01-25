8183f7efc048018bd1508f7658e594e4d14bdb8d
CaiBee233 <jinxie000@gmail.com>
2022-01-24 16:54:06 -0800

    End of Section 16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,13 +12,11 @@ namespace Persistence
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<Activity> Activities { get; set; }
-
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
-
+        public DbSet<Photo> Photos { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
