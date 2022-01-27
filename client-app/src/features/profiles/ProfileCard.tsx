8af1e5f57f52d6f998948ea6bc84ba6ee3c74607
CaiBee233 <jinxie000@gmail.com>
2022-01-27 13:36:22 -0800

    End of Section 20

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -3,12 +3,18 @@ import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 import { Card, Icon, Image } from 'semantic-ui-react';
 import { Profile } from '../../app/models/profile';
+import FollowButton from './FollowButton';
 
 interface Props {
     profile: Profile;
 }
 
 export default observer(function ProfileCard({profile}: Props) {
+    function truncate(str: string | undefined) {
+        if (str) {
+            return str.length > 40? str.substring(0,37) + '...' : str;
+        }
+    }
     return (
         <Card as={Link} to={`/profiles/${profile.username}`}>
             <Image src={profile.image || '/assets/user.png'} />
@@ -18,8 +24,9 @@ export default observer(function ProfileCard({profile}: Props) {
             </Card.Content>
             <Card.Content extra>
                 <Icon name='user' />
-                20 followers
+                {profile.followersCount} followers
             </Card.Content>
+            <FollowButton profile={profile} />
         </Card>
     )
 })
